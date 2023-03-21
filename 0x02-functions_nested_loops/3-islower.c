bdf8e1596c49924d3287d3341b2d3a7b0c55f6cf
BereketBerhanu760 <berhanub760@gmail.com>
2023-03-21 15:21:29 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,18 +7,17 @@
  *
  *@c: The character is ASCII code
  * 
- *Return:  always 0
+ *Return: 1 for lower case character
  */
-int main(void)
+int _islower(int c)
 {
-    int r;
-
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
 }
