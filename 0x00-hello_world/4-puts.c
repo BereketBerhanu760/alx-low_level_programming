49e819dbd4697f7456f5bcad7985438ceb53ede4
BereketBerhanu760 <berhanub760@gmail.com>
2023-03-20 19:26:15 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* print_sent - prints "programming is like building a multilingual puzzle"
-* Return: 0 if successful
-*/
-int print_sent(void)
+ * main - prints result
+ * Return: 0 if successful
+ */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
