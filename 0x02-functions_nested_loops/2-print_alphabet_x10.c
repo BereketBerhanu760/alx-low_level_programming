fa7a62b70969ea64d00ad7a03c2676487b66db0d
BereketBerhanu760 <berhanub760@gmail.com>
2023-03-21 14:52:46 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,10 @@ void print_alphabet_x10(void)
 {
 	int i;
 	int j;
-	for (j = 1; i <= 10; i++)
+
+	for (i = 1; i <= 10; i++)
 	{
-		for (i = 97; i <= 122; i++)
+		for (j = 97; j <= 122; j++)
 		{
 		_putchar(i);
 		}
