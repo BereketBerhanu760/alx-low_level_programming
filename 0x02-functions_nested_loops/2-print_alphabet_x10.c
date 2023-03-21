01fa69ceea5747bce15c643ccc9dd3decd3f8da2
BereketBerhanu760 <berhanub760@gmail.com>
2023-03-21 14:44:05 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints the alphabet 10 times
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+	for (j = 1; i <= 10; i++)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+		_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
