9579f475c7bcc877dfc0a945e30894f461b662b5
BereketBerhanu760 <berhanub760@gmail.com>
2023-03-23 12:51:00 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *@c: digit returned
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
