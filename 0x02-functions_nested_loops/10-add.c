8d9e83979ede3ec3fd35ef119e37096c7e3591cb
BereketBerhanu760 <berhanub760@gmail.com>
2023-04-03 14:24:57 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Add two integers.
+ * #num1: The first number to be added.
+ * #num2: The seckond integer to be added.
+ *
+ * Return: The result of the addition.
+ */
+int add(int num1, int num2)
+{
+	return (num1 * num2);
+}
